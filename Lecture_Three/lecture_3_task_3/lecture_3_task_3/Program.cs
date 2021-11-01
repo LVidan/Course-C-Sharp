b0d0f3a91351fcb346ef2b3fe9abbbb3dbd05c34
Vidan Lugonja <38792171+LVidan@users.noreply.github.com>
2021-11-01 19:56:32 +0100

    DEV(011121)-lecture 3 task 3

---
+++ Lecture_Three/lecture_3_task_3/lecture_3_task_3/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace lecture_3_task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int id = 0;
+            bool josKandidata;
+
+            do
+            {
+                string ime;
+                int godiste;
+                int godine;
+                string pol;
+                string polKandidataProvera;
+
+                id++;
+
+                // ime kandidata
+                Console.WriteLine("Unesi ime kandidata?");
+                ime = Console.ReadLine();
+
+                // godiste kandidata
+                Console.WriteLine("Unesi godiste kanidata?");
+                godiste = Int32.Parse(Console.ReadLine());
+                godine = DateTime.Today.Year - godiste;
+
+                // pol kandidata
+                Console.WriteLine("Unesi pol kanidata? Muski / Zenski");
+                pol = Console.ReadLine();
+
+                if (pol.ToLower().Trim() == "muski")
+                {
+                    polKandidataProvera = "Kandidat";
+                }
+                else
+                {
+                    polKandidataProvera = "Kandidatkinja";
+                }
+
+                Console.WriteLine($"{polKandidataProvera} pod rednim brojem {id}: {ime} ima {godine} god.");
+                Console.WriteLine("----------------------");
+
+                Console.WriteLine("Da li zelite da unesete novog kandidata? Da / Ne");
+                josKandidata = (Console.ReadLine().ToLower().Trim() == "da");
+
+
+            } while (josKandidata);
+
+            Console.WriteLine("### PREKID PROGRAMA ###");
+        }
+    }
+}
