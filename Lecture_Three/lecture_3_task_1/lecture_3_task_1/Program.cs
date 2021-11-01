e4a9fe8259b2c8d1665b2254ee07465c22f13ece
Vidan Lugonja <38792171+LVidan@users.noreply.github.com>
2021-11-01 17:14:43 +0100

    DEV-(c-sharp): tasks from second & third lectures

---
+++ Lecture_Three/lecture_3_task_1/lecture_3_task_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace lecture_3_task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 50;
+            int B = 100;
+
+            for (int i = A; i <= B; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine($"Broj {i} je paran broj");
+                }
+            }
+        }
+    }
+}
---
+++ Lecture_Three/lecture_3_task_2/lecture_3_task_2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace lecture_3_task_2
+{
+    internal class Program
+    {
+        static void Ispis(int X, int Y, int Z)
+        {
+            Console.WriteLine("=== ISPISIVANJE ===");
+            for (int i = X + 1; i < Y; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("--------------------");
+
+            for (int j = Z - 1; j > Y; j--)
+            {
+                Console.WriteLine(j);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Upisati vrednosti za A, B i C tako da A ima najmanju vrednost, B da ima srednju i C najvecu");
+            Console.WriteLine("---------------------");
+
+            // A
+            Console.WriteLine("Vrednost A:");
+            int A = Int32.Parse(Console.ReadLine());
+
+            // B
+            Console.WriteLine("Vrednost B:");
+            int B = Int32.Parse(Console.ReadLine());
+
+            // C
+            Console.WriteLine("Vrednost B:");
+            int C = Int32.Parse(Console.ReadLine());
+
+            /* A = manje od B i C
+             * B = veci od A ali mani od C
+             * C = veci i od A i od B
+            */
+            if (A < B && A < C && B > A && B < C)
+            {
+                Ispis(A, B, C);
+            }
+            else
+            {
+                Console.WriteLine($"Greska! Vrednost A:{A} mora biti manja od B:{B} i C:{C}, vrednost B mora biti manja od C a veca od A, vrednost C mora biti veca i od A i od B");
+            }
+        }
+    }
+}
