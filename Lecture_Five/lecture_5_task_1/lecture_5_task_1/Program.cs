6e2c3260fb55cc31203e6e2139cd1e972de70faa
Vidan Lugonja <38792171+LVidan@users.noreply.github.com>
2021-11-12 01:04:36 +0100

    vl-priting-star-patterns

---
+++ Lecture_Five/lecture_5_task_1/lecture_5_task_1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace lecture_5_task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, brKolona, brRedova;
+
+            /* dinamicki unos
+            * 1. br kolona
+            * 2. br redova
+            */
+            Console.WriteLine("Uneti zeljeni broj kolona?");
+            brKolona = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Uneti zeljeni broj redova?");
+            brRedova = Int32.Parse(Console.ReadLine());
+
+            // provera da korisnik ne unese PARAN broj za broj kolona
+            if (brKolona % 2 == 0)
+            {
+                Console.WriteLine("Doslo je do greske. Unesite paran broj kolona!!!");
+            }
+            else
+            {
+                for (i = 1; i <= brKolona; i++)
+                {
+                    for (j = 1; j <= brRedova; j++)
+                    {
+                        if (i % 2 == 0 && j > 1 && j < brRedova)
+                        {
+                            Console.Write(" ");
+                        }
+                        else
+                        {
+                            Console.Write('*');
+                        }
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
